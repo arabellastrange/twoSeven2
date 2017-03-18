38dee7baff0057f03ab0ad7587293f107bd7eef7
Louise MacDougall <Louise MacDougall>
2017-03-18 15:08:30 +0000

    timers

--- src/Settings.java
+++ src/Settings.java
@@ -1,7 +1,21 @@
 
 public class Settings {
-
-	public void setTimer(double time){
+	long start;
+	double current;
+	double elapsed;
+	double limit;
+	
+	public Settings(){
+		start = System.currentTimeMillis();
+	}
+	
+	public double getTime(){
+		current = System.currentTimeMillis();
+		elapsed = (current - start) / 1000.00;
 		
+		return elapsed;
+	}
+	public void setTimer(double time){
+		limit = time;
 	}
 }
--- src/Timer.java
+++ src/Timer.java
@@ -1,14 +1,5 @@
 public class Timer {
-	long start;
 	
-	public Timer(){
-		start = System.currentTimeMillis();
-	}
 	
-	public double getTime(){
-		double current = System.currentTimeMillis();
-		double elapsed = (current - start) / 1000.00;
-		
-		return elapsed;
-	}
+	
 }
