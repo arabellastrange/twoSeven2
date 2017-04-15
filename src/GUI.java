7796d1e58412f925c73f4261c2cbecd5dece19dc
chadha degachi <AHC@Shatha-PC.(none)>
2017-04-16 00:14:33 +0100

    added ai diff and player two screens!

--- src/GUI.java
+++ src/GUI.java
@@ -19,22 +19,33 @@ import java.io.IOException;
 
 public class GUI extends Frame implements ActionListener {
 	Player playerOne = new Player();
-	JFrame first;
-	JPanel welcome;
-	JTextField accountName;
+	Player playerTwo = new Player();
+	JFrame screen;
+	JLabel updates;	
 	
 	public GUI(){
 		welcome();
 	}
 	
+	//One method per screen
 	public void welcome(){	
-		first = new JFrame("Welcome");
-		welcome =  new JPanel();
+		final JTextField accountName = new JTextField(20);
+		updates = new JLabel();
+		updates.setText("Watch this sace for feedback through out your game!");
+		screen = new JFrame("Welcome");
+		JPanel welcome =  new JPanel();
 		
 		JButton start = new JButton("Start");
-		start.addActionListener(this);
+		start.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String account = accountName.getText();
+				playerOne.setName(account);
+				welcome.setVisible(false);
+				updates.setText("Player one created!");
+				choose(account);	
+			}
+		});
 		
-		accountName = new JTextField(20);
 		JLabel message = new JLabel();
 		message.setText("Welcome to Kamisado, please enter your name");
 		
@@ -43,40 +54,95 @@ public class GUI extends Frame implements ActionListener {
 		welcome.add(start);
 		welcome.setVisible(true);
 		
-		first.add(welcome);
-		first.setVisible(true);
-		first.setSize(300, 150);
-		first.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		screen.add(welcome);
+		//screen.add(updates); --> overwrite panel deal with it
+		screen.setVisible(true);
+		//screen.setSize(300, 150);
+		screen.pack();
+		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		String account = accountName.getText();
-		playerOne.setName(account);
-		welcome.setVisible(false);
-		choose(account);	
-	}
 	
 	public void choose(String account){
-		JPanel choice;
-		choice = new JPanel();
+		JPanel choice = new JPanel();
 		
 		JLabel message = new JLabel();
 		message.setText("Hi " + account + " do you wish to play against Human or AI?");
 		
 		JButton human = new JButton("Human");
 		JButton ai = new JButton("AI");
-		human.addActionListener(this);
-		ai.addActionListener(this);
+		human.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String opOption = "H";
+				choice.setVisible(false);
+				updates.setText("You are playing against another human.");
+				playerTwo();
+			}
+		});
+		
+		ai.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String opOption = "A";
+				choice.setVisible(false);
+				updates.setText("You are playing against AI");
+				diffculty();
+			}
+		});
 		
 		choice.add(message);
 		choice.add(human);
 		choice.add(ai);
 		choice.setVisible(true);
-		first.add(choice);
+		screen.add(choice);
+	}
+	
+	public void diffculty(){
+		JPanel diffculty = new JPanel();
+		JLabel message = new JLabel();
+		message.setText("Select a diffculty level!");
+		JButton easy = new JButton("Easy");
+		JButton hard = new JButton("Hard");
+		
+		easy.addActionListener(this);
+		hard.addActionListener(this);
+		
+		diffculty.add(message);
+		diffculty.add(easy);
+		diffculty.add(hard);
+		diffculty.setVisible(true);
+		screen.add(diffculty);
+		
+	}
+	
+	public void playerTwo(){
+		final JTextField oppName = new JTextField(20);
+		JPanel playertwo = new JPanel();
+		
+		JLabel message = new JLabel();
+		message.setText("Player two please enter you name: ");
+		
+		JButton next = new JButton("Next");
+		next.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String account = oppName.getText();
+				playerTwo.setName(account);
+				playertwo.setVisible(false);
+				updates.setText("Player two created!");	
+			}
+		});
+		
+		playertwo.add(message);
+		playertwo.add(oppName);
+		playertwo.add(next);
+		playertwo.setVisible(true);
+		screen.add(playertwo);
+		
+	}
 
-		first.setSize(350, 150);
-		first.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
